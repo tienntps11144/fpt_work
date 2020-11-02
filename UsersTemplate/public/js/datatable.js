8563a707ad03e517ca0a1d640eac16dae32c69e3
tiennt <tienntps11144@fpt.edu.vn>
2020-11-02 16:14:54 +0700

    dashboard

--- UsersTemplate/public/js/datatable.js
+++ UsersTemplate/public/js/datatable.js
@@ -7,6 +7,7 @@ function createDataTable(tableId, optionSetting){
     } );
 }
 createDataTable('student-table', [{ targets: [ 0 ], searchable: false }, { targets: [ 4 ], orderData: false }]);
+createDataTable('post-table', [{ targets: [ 0 ], searchable: false }, { targets: [ 5 ], orderData: false }]);
 createDataTable('company-table', [{ targets: [ 0 ], searchable: false }, { targets: [ 5 ], orderData: false }]);
 createDataTable('tag-table', [{ targets: [ 0 ], searchable: false }, { targets: [ 3 ], orderData: false }]);
 createDataTable('cate-table', [{ targets: [ 0 ], searchable: false }, { targets: [ 2 ], orderData: false }]);
